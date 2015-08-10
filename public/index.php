9ab86b683be3f93b9814136ec3ec79f2f669372f
dgcollier <david@gcollier.me>
2015-08-10 15:17:45 -0500

    Removed old index not in public.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,45 @@
 <html>
 <head>
     <title>Tool Shed</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <style type="text/css">
+        h1 {
+            text-align: center
+        }
+    </style>
 </head>
 <body>
+    <header>
+
+
+
+
+
+
+
+
+
+    </header>
+    
     <h1>Tool Shed</h1>
 
+
+
+
+
+
+
+
+
+
+    
+    <footer>
+
+
+
+
+
+
+    </footer>
 </body>
 </html>

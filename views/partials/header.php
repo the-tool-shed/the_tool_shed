47eee5115c3191138f8c3505f538eae9b62dde12
dgcollier <david@gcollier.me>
2015-08-11 11:48:23 -0500

    extracted header from ads.index.php

---
+++ views/partials/header.php
@@ -0,0 +1,23 @@
+<div class='container'>
+    <header class="navbar navbar-default navbar-fixed-top">
+        <div>
+            <div id='logo'>
+                <a href='#'>LOGO Goes Here</a>
+            </div>
+            <div id='center-search'>
+                <form>
+                    <input type='text'>
+                    <button>Search</button>
+                </form>
+            </div>
+            <div id='right-side'>
+                <div id='register'>
+                    <a href='#'>Register</a>
+                </div>
+                <div id='login'>
+                    <a href='#'>Login</a>
+                </div>
+            </div>
+        </div>
+    </header>
+<div>

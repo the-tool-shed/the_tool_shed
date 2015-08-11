339d94f3965a5e0123ac0db478e5dfb0a0df7914
Christian Tobias <cwtobias6@gmail.com>
2015-08-10 19:57:19 -0500

    fixed header and footer

--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
     <title>Tool Shed</title>
     <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
     <style type="text/css">
+        body {
+            text-align: center;
+        }
         #indexTitle {
             text-align: center;
             font-size: 4em;
@@ -31,8 +34,21 @@
             padding:5px;
 
         }
+        #right-side {
+            display:inline-block;
+            padding:5px;
+            float:right;
+            margin-left: 1000px;
+
+        }
+        #logo {
+            
+            padding:10px;
+
+        }
         footer div {
             display: inline-block;
+            padding: 15px;
         }
     </style>
 </head>
@@ -68,9 +84,9 @@
     
     <footer class="navbar navbar-default navbar-fixed-bottom">
         <div class="container">
-            <div><a href='#'>&copy The Tool Shed</a></div>
-            <div><a href='#'>About</a></div>
+            <div><a href='#'>About The Tool Shed</a></div>
             <div><a href='#'>Terms of Use</a></div>
+            <div><a href='#'>&copy The Tool Shed. All Rights Reserved</a></div>
             
             
         </div>

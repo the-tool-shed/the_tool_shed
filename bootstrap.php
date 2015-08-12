e75943efdcd68462c870257c1ea06e9e4b575f97
dgcollier <david@gcollier.me>
2015-08-12 15:13:36 -0500

    Fixed bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,5 @@ require_once "models/BaseModel.php";
 require_once "utils/Input.php";
 require_once "utils/Auth.php";
 require_once "views/partials/footer.php";
-require_once "../models/Ad.php";
-require_once "../models/Users.php";
+require_once "models/Ad.php";
+require_once "models/User.php";
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php  
 
 require_once "../bootstrap.php";
+
 require_once "../views/partials/indexheader.php";
 
 ?>

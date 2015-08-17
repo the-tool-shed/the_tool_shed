d4487d03bfd9636ff5a1c34924847f7dc9eff26d
dgcollier <david@gcollier.me>
2015-08-17 10:43:08 -0500

    Added background images to jumbotrons. Added new jumbotrons. Removed search bar.

--- public/index.php
+++ public/index.php
@@ -60,17 +60,48 @@ require_once "../views/partials/indexheader.php";
         h1 {
             color:darkorange;
         }
+
+        #jumbo1 {
+            height: 750px;
+            background-image: url("/img/boots.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+            vertical-align: middle;
+        }
+
+        #jumbo2 {
+            background-color: #f5f5f5;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        #jumbo3 {
+            background-image: url("/img/chalkboard.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
     </style>
 </head>
 <body>
     
-    <div class='jumbotron'>
+    <div class='jumbotron' id="jumbo1">
         <h1 id="indexTitle">Tool Shed</h1>
-        <form id="mainForm" action="/ads.index.php">
-                <p id="mainFormLabel">I want to learn...</p>
-                <input id="mainSearch" type="search" name="search" placeholder="x" autofocus>
-                <input id="searchSubmit" type="submit">
-        </form>
+        <a href="ads.index.php"><button class="btn btn-lg btn-warning">ENTER</button></a>
+    </div>
+    <div class='jumbotron' id="jumbo2">
+        <h2>Section A</h2>
+        <ul>
+            <li>Thing 1</li>
+            <li>Thing 2</li>
+            <li>Thing 3</li>
+        </ul>
+    </div>
+    <div class='jumbotron' id="jumbo3">
+        <h2>Section B</h2>
+        <p>Paragraph here</p>
+        <p>Paragraph here</p>
+        <p>Paragraph here</p>
+        <p>Paragraph here</p>
     </div>
 </body>
 </html>
